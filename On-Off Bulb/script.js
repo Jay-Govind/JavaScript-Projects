c882eee5c255d017900273b005f1acec0214e84e
Jay-Govind <govind.iq@gmail.com>
2024-04-24 23:49:33 +0530

    Code for Bulb

---
+++ On-Off Bulb/script.js
@@ -0,0 +1,9 @@
+const bulb = document.getElementById("bulb_img");
+
+function ONfunction () {
+      bulb.src = "https://www.w3schools.com/html/pic_bulbon.gif";
+}
+
+function OFFfunction () {
+      bulb.src = "https://www.w3schools.com/html/pic_bulboff.gif";
+}
