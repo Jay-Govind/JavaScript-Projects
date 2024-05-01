a854be2bec0df2076d7663abce418cd8491951e8
Jay-Govind <govind.iq@gmail.com>
2024-05-01 23:58:49 +0530

    fan project

---
+++ Fan Project/script.js
@@ -0,0 +1,21 @@
+const fan = document.getElementById("fan");
+
+function ONfun() {
+      fan.style.animationDuration = 3+"s";      
+}
+
+function OFFfun() {
+      fan.style.animationDuration = 0+"s";  
+}
+
+function fun1() {
+      fan.style.animationDuration = 1+"s";  
+}
+
+function fun2() {
+      fan.style.animationDuration = 0.7+"s";  
+}
+
+function fun3() {
+      fan.style.animationDuration = 0.3+"s";  
+}
